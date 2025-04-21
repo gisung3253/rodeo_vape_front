f91e2cd50fb4bc43fe7a05fc5e42af2d1da96be6
gisung3253 <gisung3253@naver.com>
2025-04-21 20:13:29 +0900

    app.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import axios from 'axios';
 import Layout from './components/layout/Layout';
 import Login from './pages/Login';
 import Sales from './pages/sales/index';
@@ -12,27 +10,6 @@ import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    // 토큰이 있으면 모든 axios 요청에 헤더 추가
-    const token = localStorage.getItem('token');
-    if (token) {
-      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    }
-    
-    // axios 응답 인터셉터 추가 - 401 오류(인증 실패) 처리
-    axios.interceptors.response.use(
-      response => response,
-      error => {
-        if (error.response && error.response.status === 401) {
-          // 토큰이 유효하지 않으면 로그인 정보 삭제 후 로그인 페이지로 리다이렉트
-          localStorage.removeItem('token');
-          window.location.href = '/login';
-        }
-        return Promise.reject(error);
-      }
-    );
-  }, []);
-
   return (
     <Router>
       <Routes>
@@ -54,7 +31,7 @@ function App() {
           <Navigate to="/sales" /> :
           <Navigate to="/login" />
         } />
-
+        
         {/* 404 페이지 - 없는 경로는 루트로 리다이렉트 */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
